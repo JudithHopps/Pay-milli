c50012f6bf388348ae25b63a7d1de611350f44d8
gywns2zang9 <gywns2zang9@gmai.com>
2024-09-05 11:24:10 +0900

    feat: navbar 스타일 추가

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,27 +1,51 @@
 import { Link } from "react-router-dom";
-import "../../styles/components/NavBar.css";
-import logo1 from "../../assets/logo1.png";
-import logo2 from "../../assets/logo2.png";
+import styled from "styled-components";
+import logo from "../../assets/logo.png";
 
 function NavBar() {
   return (
-    <nav className="navbar">
-      <div className="logo-container">
-        <Link to="/" className="logo-link">
-          <img src={logo1} alt="Pay-milli 로고" className="logo" />
-          <span className="app-name">Pay-milli</span>
-        </Link>
-      </div>
-      <div className="auth-links">
-        <Link to="/login" className="link">
-          로그인
-        </Link>
-        <Link to="/signup" className="link">
-          회원가입
-        </Link>
-      </div>
-    </nav>
+    <StyledNav>
+      <LogoContainer>
+        <StyledLink to="/">
+          <AppLogo src={logo} alt="Pay-milli 로고" />
+        </StyledLink>
+      </LogoContainer>
+      <Links>
+        <StyledLink to="/">홈</StyledLink>
+        <StyledLink to="/login">로그인</StyledLink>
+        <StyledLink to="/signup">회원가입</StyledLink>
+      </Links>
+    </StyledNav>
   );
 }
 
 export default NavBar;
+
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+`;
+
+const LogoContainer = styled.div``;
+
+const AppLogo = styled.img`
+  height: 40px;
+  border-radius: 10px;
+`;
+
+const Links = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-size: 13px;
+  color: gray;
+  &:hover {
+    color: #6dcef5;
+  }
+`;
