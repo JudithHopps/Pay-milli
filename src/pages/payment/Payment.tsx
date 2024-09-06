be418ab99f89ccb54dd4d15761b7de5da433f7a4
JudithHopps <kookjeayon100@gmail.com>
2024-09-06 15:39:02 +0900

    feat : paymentPassword 컴포넌트 버튼 구성, 완성 로직 추가

--- src/pages/payment/Payment.tsx
+++ src/pages/payment/Payment.tsx
@@ -156,6 +156,7 @@ export default function Payment() {
   };
 
   const handlePasswordSubmit = (password: string) => {
+    // todo : 비밀번호 확인 로직 추가!!
     if (password === "1234") {
       // Replace with proper validation logic
       console.log("Password correct. Proceed with payment:", cardAllocations);
@@ -165,6 +166,7 @@ export default function Payment() {
     } else {
       alert("비밀번호가 틀렸습니다.");
       setIsProcessing(false);
+      setShowPassword(false);
     }
   };
 
