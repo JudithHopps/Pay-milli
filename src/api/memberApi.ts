1965f8b3360c5d0b128cd827c4228d04abba1357
gywns2zang9 <gywns2zang9@gmai.com>
2024-09-12 16:54:39 +0900

    feat: member api 추가

--- src/api/memberApi.ts
+++ src/api/memberApi.ts
@@ -1,5 +1,9 @@
 import axiosInstance from "./axiosInstance";
-import { SignupFormData, LoginFormData } from "../types/memberTypes";
+import {
+  SignupFormData,
+  LoginFormData,
+  MemberInfoData,
+} from "../types/memberTypes";
 
 // 회원가입 API
 export const signup = async (formData: SignupFormData) => {
@@ -37,5 +41,59 @@ export const getMemberInfo = async (accessToken: string) => {
       Authorization: `Bearer ${accessToken}`,
     },
   });
+  return response.data;
+};
+
+// 토큰 재발급 API
+export const refreshToken = async (accessToken: string) => {
+  const response = await axiosInstance.post(
+    "/member/refresh",
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
+  );
+  return response.data;
+};
+
+// 회원 탈퇴 API
+export const deleteMember = async (accessToken: string) => {
+  const response = await axiosInstance.delete("/member", {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  return response.data;
+};
+
+// 회원 정보 변경 API
+export const updateMemberInfo = async (
+  accessToken: string,
+  formData: Partial<MemberInfoData>,
+) => {
+  const response = await axiosInstance.put("/member", formData, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  return response.data;
+};
+
+// 결제 비밀번호 변경 API
+export const updatePaymentPassword = async (
+  accessToken: string,
+  paymentPassword: string,
+) => {
+  const response = await axiosInstance.put(
+    "/member/payment/password",
+    { paymentPassword },
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
+  );
   return response.data;
 };
