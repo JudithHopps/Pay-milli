494b5bd7ea761aee879d130d00149fa99b2cd155
gywns2zang9 <gywns2zang9@gmai.com>
2024-09-27 09:39:08 +0900

    feat: env파일에서 baseurl 관리

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
-const BASE_URL = "http://j11a702.p.ssafy.io/api/v1/paymilli";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  //baseURL: process.env.BASE_URL,
   headers: {
     "Content-Type": "application/json; charset=utf-8",
   },
